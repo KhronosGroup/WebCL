aeae354e004bfa1209c3b985639b02e20db53657
steveneliuk <s.eliuk@samsung.com>
2013-11-14 15:32:48 -0800

    deleting bad initial commit of wrong webcl repo from 2012

--- latest/resources/generateWebIDL.js
+++
@@ -1,8 +0,0 @@
-// Extracts WebIDL code from <pre class="idl"> elements
-
-function generateWebIDL(document) {
-  var s = Array.prototype.slice.call(document.querySelectorAll("pre.idl")).map(function(n) { return n.textContent; }).join('\n\n'); 
-  document.open("text/plain");
-  document.write(s);
-  document.close();
-}
